196160db5acd1fa2642c3def66511e6ea80dba32
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-08-25 13:29:55 -0300

    amendd

--- turf_plants_project/game_api/views.py
+++ turf_plants_project/game_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Pixel, Player
 from django.db.models import F
 from decouple import config
