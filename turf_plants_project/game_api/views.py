5317795c13044542fa84dd50a9bbd67d53888055
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-08-25 13:40:21 -0300

    everything is terrible

--- turf_plants_project/game_api/views.py
+++ turf_plants_project/game_api/views.py
@@ -48,11 +48,15 @@ def paint_pixel(request):
 
     return Response(pixel_data)
 
+log = logging.getLogger(__name__)
+
 @api_view(['POST'])
 @authentication_classes([])
 def award_hourly_xp(request):
-    print("HOLAAA",config('ADMIN_TOKEN'))
+    log.info("HOLAAA",config('ADMIN_TOKEN'))
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
+    log.info("ASJDAKSJDKJA",token)
+    log.info("UWUWU",token==config('ADMIN_TOKEN'))
     if token == config('ADMIN_TOKEN'):
         updated = Pixel.objects.update(total_xp=F('total_xp') + 1)
         return Response({"success": True, "updated_pixels": updated})
