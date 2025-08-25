44eb1ec268b65c3a4763e6a1fe627a9d2cbe668c
SrRenho <juanjogvizioli@gmail.com>
2025-08-25 01:34:05 -0300

    amend

--- turf_plants_project/game_api/urls.py
+++ turf_plants_project/game_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('pixels/', views.get_pixels),
     path('paint/', views.paint_pixel),
+    path('award_hourly_xp/', views.award_hourly_xp, name='award_hourly_xp'),
 ]
